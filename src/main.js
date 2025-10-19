ba30a3b5a85ac9b09d47188abcd68f35086552b9
xxFrancesco <cardiafrancesco30@gmail.com>
2025-10-19 18:02:15 +0200

    on

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
-const CLOUD_PROJECT_NUMBER = '418566026845';
-const MAIN_STAGE_URL = 'https://fcardia.github.io/meet_extension_example/src/MainStage.html';
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-async function setUpAddon() {
-  // ✅ Usa l’oggetto globale fornito dalla libreria gstatic
-  const session = await google.workspace.meet.addon.createAddonSession({
+import { meet } from '../node_modules/googleworkspace/meet-addons/meet.addons';
+
+const CLOUD_PROJECT_NUMBER = '418566026845';
+const MAIN_STAGE_URL = "https://fcardia.github.io/meet_extension_example/src/MainStage.html";
+
+/**
+ * Prepares the Add-on Side Panel Client, and adds an event to launch the
+ * activity in the main stage when the main button is clicked.
+ */
+export async function setUpAddon() {
+  const session = await meet.addon.createAddonSession({
     cloudProjectNumber: CLOUD_PROJECT_NUMBER,
   });
-
   const sidePanelClient = await session.createSidePanelClient();
-
   document
     .getElementById('start-activity')
     .addEventListener('click', async () => {
@@ -16,5 +33,13 @@ async function setUpAddon() {
     });
 }
 
-// Avvia subito la configurazione
-setUpAddon();
+/**
+ * Prepares the Add-on Main Stage Client, which signals that the add-on has
+ * successfully launched in the main stage.
+ */
+export async function initializeMainStage() {
+  const session = await meet.addon.createAddonSession({
+    cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+  });
+  await session.createMainStageClient();
+}
